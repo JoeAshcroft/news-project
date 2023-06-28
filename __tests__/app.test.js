25c807d172898ef7f29e857f30dc1aa4b57bf73c
KeeseRex <joeashcroft@hotmail.co.uk>
2023-06-28 12:13:13 +0100

    update test to match object

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -48,17 +48,16 @@ describe("GET /api/articles/:article_id", () => {
       .get("/api/articles/1")
       .expect(200)
       .then(({ body }) => {
-        expect(body.article).toHaveProperty("article_id", expect.any(Number));
-        expect(body.article).toHaveProperty("author", expect.any(String));
-        expect(body.article).toHaveProperty("title", expect.any(String));
-        expect(body.article).toHaveProperty("topic", expect.any(String));
-        expect(body.article).toHaveProperty("body", expect.any(String));
-        expect(body.article).toHaveProperty("votes", expect.any(Number));
-        expect(body.article).toHaveProperty(
-          "article_img_url",
-          expect.any(String)
-        );
-        expect(body.article).toHaveProperty("created_at", expect.any(String));
+        expect(body.article).toMatchObject({
+          article_id: expect.any(Number),
+          author: expect.any(String),
+          title: expect.any(String),
+          topic: expect.any(String),
+          body: expect.any(String),
+          votes: expect.any(Number),
+          article_img_url: expect.any(String),
+          created_at: expect.any(String),
+        });
       });
   });
 });
